e7559f31d61ddd541c98f2e78a11abb56491099d
lofeodo <daniel.lofeodo@gmail.com>
2024-11-23 16:21:37 -0500

    Make IntegerBoard work for any size.

--- IntegerBoard.java
+++ IntegerBoard.java
@@ -1,4 +1,3 @@
-
 /**
  * IntegerBoard is an implementation of GameBoard using
  * Integers as entries
@@ -9,16 +8,18 @@
  */
 
 public class IntegerBoard implements GameBoard<Integer> {
-    private final int width = 9;
-    private final int height = 9;
+    private final int width;
+    private final int height;
+    private final int subsquareSize;
     private final Integer[][] board;
-    private boolean valid = true;
 
     public IntegerBoard(Integer[][] board) {
-        if (board.length != height || board[0].length != width) {
-            System.out.println("Illegal dimensions.");
-            valid = false;
-            this.board = null;
+        height = board.length;
+        width = board[0].length;
+        subsquareSize = getSubsquareSize(height);
+        if (!isBoardSizeValid(width, height, subsquareSize)) {
+            throw new IllegalArgumentException(
+                    "Illegal dimensions. Board's height and width must be equal and be square-rootable.");
         } else {
             this.board = board;
         }
@@ -47,21 +48,13 @@ public class IntegerBoard implements GameBoard<Integer> {
 
     @Override
     public Integer getCell(int row, int col) throws IndexOutOfBoundsException {
-        if (!valid)
-            return null;
-
         checkBounds(row, col);
-
         return board[row][col];
     }
 
     @Override
     public void setCell(int row, int col, Integer val) throws IndexOutOfBoundsException {
-        if (!valid)
-            return;
-
         checkBounds(row, col);
-
         board[row][col] = val;
     }
 
@@ -75,13 +68,12 @@ public class IntegerBoard implements GameBoard<Integer> {
         return height;
     }
 
+    public int getSubsquareSize() {
+        return subsquareSize;
+    }
+
     @Override
     public void display() {
-        if (!valid) {
-            System.out.println("Board is invalid - cannot be displayed.");
-            return;
-        }
-
         System.out.print("-------------------------");
         System.out.println();
         for (int row = 0; row < height; row++) {
@@ -105,5 +97,20 @@ public class IntegerBoard implements GameBoard<Integer> {
         if (row < 0 || row >= width || col < 0 || col >= height) {
             throw new IndexOutOfBoundsException("Cell position out of bounds.");
         }
+    }
+
+    private int getSubsquareSize(int boardSize) {
+        return (int) Math.sqrt(boardSize);
+    }
+
+    private boolean isBoardSizeValid(int boardWidth, int boardHeight, int subsquareSize) {
+        // Board must be square.
+        if (boardWidth != boardHeight)
+            return false;
+        // Board size must be squareroot-able
+        if (subsquareSize * subsquareSize != boardWidth)
+            return false;
+
+        return true;
     }
 }
